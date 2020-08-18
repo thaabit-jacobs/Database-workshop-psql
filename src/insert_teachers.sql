ccb2fa6c1103b49effeaa3263fdc0324bd4e59ed
Moahmmed Thaabit <mohammedthaabitjacobs@gmail.com>
2020-08-18 20:03:05 +0200

    insert teachers

---
+++ src/insert_teachers.sql
@@ -0,0 +1,6 @@
+insert into teacher (first_name, last_name, email) values ('Lindani', 'Pani', 'lindani@email.com');
+insert into teacher (first_name, last_name, email) values ('Siba', 'Khumalo', 'siba@gmail.com');
+insert into teacher (first_name, last_name, email) values ('Thaabit', 'Jacobs', 'thaabit@email.com');
+insert into teacher (first_name, last_name, email) values ('Kauthar', 'Orrie', 'orrie@gmail.com');
+insert into teacher (first_name, last_name, email) values ('Jimmy', 'legs', 'legs@gmail.com');
+insert into teacher (first_name, last_name, email) values ('John', 'Bane', 'bane@gmail.com');
