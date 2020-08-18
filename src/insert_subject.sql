0964da05c637017377426a675cfc667db99e41ec
Moahmmed Thaabit <mohammedthaabitjacobs@gmail.com>
2020-08-18 20:11:56 +0200

    insert subjects

---
+++ src/insert_subject.sql
@@ -0,0 +1,7 @@
+insert into subject (name) values ('Mathmatics');
+insert into subject (name) values ('English');
+insert into subject (name) values ('Afrikaans');
+insert into subject (name) values ('Life Science');
+insert into subject (name) values ('Geography');
+insert into subject (name) values ('Physical Science');
+insert into subject (name) values ('Physical Education');
