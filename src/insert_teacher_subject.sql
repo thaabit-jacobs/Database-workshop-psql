221eb4817e91cb889c5a30940978750c2eb61922
Moahmmed Thaabit <mohammedthaabitjacobs@gmail.com>
2020-08-18 20:32:28 +0200

    teachers_subjects

---
+++ src/insert_teacher_subject.sql
@@ -0,0 +1,9 @@
+insert into teacher_subject (teacher_id, subject_id) values (1, 1);
+insert into teacher_subject (teacher_id, subject_id) values (1, 6);
+insert into teacher_subject (teacher_id, subject_id) values (2, 4);
+insert into teacher_subject (teacher_id, subject_id) values (3, 5);
+insert into teacher_subject (teacher_id, subject_id) values (3, 6);
+insert into teacher_subject (teacher_id, subject_id) values (4, 7);
+insert into teacher_subject (teacher_id, subject_id) values (5, 3);
+insert into teacher_subject (teacher_id, subject_id) values (6, 2);
+insert into teacher_subject (teacher_id, subject_id) values (6, 3);
